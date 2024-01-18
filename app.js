54ecb511cb19535490468109a8f4e823e8e42b9a
sebastian sovier <sebastian.sovier@tgtgroup.cl>
2024-01-18 17:03:56 -0300

    fix

--- app.js
+++ app.js
@@ -11,25 +11,21 @@ const corsOptions = {
   origin: ['http://18.117.196.27:80','http://ec2-18-117-196-27.us-east-2.compute.amazonaws.com:80','ec2-18-117-196-27.us-east-2.compute.amazonaws.com','http://ec2-18-117-196-27.us-east-2.compute.amazonaws.com','http://localhost:4200'],
   optionsSuccessStatus: 200 || 204
 }
+
 app.disable('x-powered-by');
 app.use(cors(corsOptions));
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, "files")));
-// parse application/json
 app.use(bodyParser.json())
-
 app.use('/Account', AccountRoutes);
 app.use('/Countries', PaisesRoutes);
 app.use('/Ciudades', CiudadesRoutes);
-
-/* Error handler middleware */
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   console.error(err.message, err.stack);
   res.status(statusCode).json({'message': err.message});
   return;
 });
-
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 });
