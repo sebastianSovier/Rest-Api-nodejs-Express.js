4d8a1270c6b460c59c12d3851c4ae8da87a4b345
sebastian sovier <sebastian.sovier@tgtgroup.cl>
2024-02-12 19:12:57 -0300

    firebase admin create find user generate token

--- routes/accountController.js
+++ routes/accountController.js
@@ -5,6 +5,14 @@ const bcrypt = require('bcrypt');
 const saltRounds = 10;
 const axios = require('axios').default;
 const https = require('https');
+const admin = require("firebase-admin");
+const serviceAccount = require("../path/proyecto-angular-12-firebase-adminsdk-3b0cj-ba2223cc30.json");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://proyecto-angular-12-default-rtdb.firebaseio.com"
+});
+const auth = admin.auth();
+
 require('dotenv').config();
 const instance = axios.create({
   baseURL: process.env.URLCORELOCAL,
@@ -14,6 +22,9 @@ const instance = axios.create({
   responseType: "json",
   headers: { "Content-Type": "application/json" }
 });
+
+
+
 router.post('/Login', async function (req, res) {
   err = "";
   console.log(req.body);
@@ -23,17 +34,43 @@ router.post('/Login', async function (req, res) {
     console.log(req.Username + "  " + req.Password);
     err = "invalid";
   }
-  if (err === "invalid") return res.status(500).send("There was a problem validando the user.")
-
-
-
+  if (err === "invalid") return res.status(500).send("There was a problem validating the user.")
   instance.post('/Account/Login',
     request
   ).then(function (response) {
-    console.log(response.data);
+    console.log(response.data.data);
     if (response && response.data) {
       if (response.data.auth == true) {
-        return res.status(200).send({ data: helper.encrypt(JSON.stringify({ auth: response.data.auth, access_Token: response.data.access_Token })) });
+        admin.auth().getUserByEmail(response.data.correo)
+          .then((userRecord) => {
+            auth.createCustomToken(userRecord.uid)
+              .then((customToken) => {
+                return res.status(200).send({ data: helper.encrypt(JSON.stringify({ auth: response.data.auth, access_Token: response.data.access_Token, tokenFirebase: customToken })) });
+              })
+              .catch((error) => {
+                console.error('Error al crear el token personalizado:', error);
+              });
+          })
+          .catch((error) => {
+            admin.auth().createUser({
+              email: response.data.correo,
+              password: request.Password,
+            })
+              .then((userRecord) => {
+                console.log('Successfully created new user:', userRecord.uid);
+                auth.createCustomToken(userRecord.uid)
+                  .then((customToken) => {
+                    console.log('Token personalizado:', customToken);
+                    return res.status(200).send({ data: helper.encrypt(JSON.stringify({ auth: response.data.auth, access_Token: response.data.access_Token, tokenFirebase: customToken })) });
+                  })
+                  .catch((error) => {
+                    console.error('Error al crear el token personalizado:', error);
+                  });
+              })
+              .catch((error) => {
+                console.error('Error creating user:', error);
+              });
+          });
       } else {
         console.log("aqui1");
         return res.status(403).send({ data: helper.encrypt(JSON.stringify({ Error: "98", auth: false, mensaje: "Usuario o contrasena incorrecto" })) });
@@ -46,7 +83,7 @@ router.post('/Login', async function (req, res) {
     .catch(function (error) {
       console.log(error);
       helper.logger.error(error);
-      return res.status(500).send({ data: helper.encrypt(JSON.stringify({ Error: "98", auth: false, mensaje: "Usuario o contraseña incorrecto" })) });
+      return res.status(200).send({ data: helper.encrypt(JSON.stringify({ Error: "98", auth: false, mensaje: "Usuario o contraseña incorrecto" })) });
     })
     .finally(function () {
     });
